95cb9fd15b026b3f094bc5b10e4ec472c350a910
syuheifujita <syuheifujita0117@gmail.com>
2020-07-31 14:28:23 +0900

    九九のスペースをそろえる

--- Main.java
+++ Main.java
@@ -6,7 +6,12 @@ public class Main {
         
         for(int i = 1; i <= 9; i++) {
             for(int j = 1; j <= 9; j++) {
-                System.out.print(i * j + " ");
+                int number = i * j;
+                if(String.valueOf(number).length() == 1) {
+                    System.out.print("  " + i * j);
+                } else {
+                    System.out.print(" " + i * j);
+                }
             }
             System.out.println("");
         }
